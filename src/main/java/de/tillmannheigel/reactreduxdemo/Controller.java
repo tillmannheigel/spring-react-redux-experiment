8768582d63fe6a0e3afd51d47c6f9340e1642b15
Tillmann Heigel <tillmann.heigel@idealo.de>
2020-05-25 22:12:45 +0200

    Introduce State Object

--- src/main/java/de/tillmannheigel/reactreduxdemo/Controller.java
+++ src/main/java/de/tillmannheigel/reactreduxdemo/Controller.java
@@ -3,12 +3,20 @@ package de.tillmannheigel.reactreduxdemo;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.tillmannheigel.reactreduxdemo.model.State;
+
 @org.springframework.stereotype.Controller
 public class Controller {
 
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("data", "server-data");
+    public String index(Model model) throws JsonProcessingException {
+        State defaultState = State.builder().build();
+        model.addAttribute("state", OBJECT_MAPPER.writeValueAsString(defaultState));
         return "index";
     }
 }
---
+++ src/main/java/de/tillmannheigel/reactreduxdemo/model/State.java
@@ -0,0 +1,16 @@
+package de.tillmannheigel.reactreduxdemo.model;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Singular;
+import lombok.Value;
+
+@Value
+@Builder
+public class State {
+    @Singular
+    List<String> filters;
+    @Builder.Default
+    String sort = "default";
+}
