79d1277327dd0a38ac301c613644cda4a30a2be2
Tillmann Heigel <tillmann.heigel@idealo.de>
2020-05-25 22:31:21 +0200

    Use State for Frontend

--- src/main/webapp/WEB-INF/js/index.js
+++ src/main/webapp/WEB-INF/js/index.js
@@ -1,7 +1,11 @@
+import $ from 'jquery';
+
 class Index {
 
     constructor() {
         console.log("Start React");
+        let state = JSON.parse($("#state")[0].innerText);
+        console.log(state);
     }
 }
 
